a92e472dfe1eca40f2ad4285b8ca5c6b75799f9c
GuillemAdell <109770845+GuillemAdell@users.noreply.github.com>
2022-07-22 19:40:46 +0200

    Add files via upload

---
+++ Tema 4/ejercicio2.py
@@ -0,0 +1,6 @@
+numero_inical = 2
+numero_final = 8
+
+for numero in range(numero_inical, numero_final):
+    if numero % 2 != 0:
+        print(numero)
---
+++ Tema 4/ejercico3.py
@@ -0,0 +1,5 @@
+from audioop import reverse
+
+
+for numero in reversed(range(1, 101)):
+    print(numero)
